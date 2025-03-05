f88986fa8a590becb970d9df67ab77790c264d55
biggestlad <ABO24002278@HeartofYorkshire.ac.uk>
2025-03-05 00:04:27 +0000

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float walkSpeed = 4f;
+    [SerializeField] private float sprintSpeed = 7f;
+    [SerializeField] private float gravity = 9.81f;
+
+    [Header("Camera Settings")]
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float bobFrequency = 10f;
+    [SerializeField] private float bobAmount = 0.05f;
+    [SerializeField] private float mouseSensitivity = 2f;
+
+    private Vector3 velocity;
+    private float defaultCameraY;
+    private float bobTimer;
+    private Rigidbody rb;
+    private float rotationX = 0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        defaultCameraY = cameraTransform.localPosition.y;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleHeadBob();
+        HandleCameraMovement();
+    }
+
+    void HandleMovement()
+    {
+        float moveSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed;
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        rb.velocity = new Vector3(move.x * moveSpeed, rb.velocity.y - gravity * Time.deltaTime, move.z * moveSpeed);
+    }
+
+    void HandleCameraMovement()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+        
+        cameraTransform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    void HandleHeadBob()
+    {
+        if (rb.velocity.magnitude > 0.1f)
+        {
+            bobTimer += Time.deltaTime * bobFrequency;
+            float bobOffset = Mathf.Sin(bobTimer) * bobAmount;
+            cameraTransform.localPosition = new Vector3(
+                cameraTransform.localPosition.x,
+                defaultCameraY + bobOffset,
+                cameraTransform.localPosition.z
+            );
+        }
+        else
+        {
+            bobTimer = 0;
+            cameraTransform.localPosition = new Vector3(
+                cameraTransform.localPosition.x,
+                Mathf.Lerp(cameraTransform.localPosition.y, defaultCameraY, Time.deltaTime * 10f),
+                cameraTransform.localPosition.z
+            );
+        }
+    }
+}
